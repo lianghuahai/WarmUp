ab75f22f0c7f934b1f47d1cb749d81b7a561fe8c
lianghuahai <huahai.liang@stonybrook.edu>
2018-06-23 09:38:58 -0400

    a

--- Algorithm/sorting/RadixSort.java
+++ Algorithm/sorting/RadixSort.java
@@ -1,5 +1,38 @@
 package sorting;
 
-public class RadixSort {
+import java.util.ArrayList;
+import java.util.List;
 
+public class RadixSort {
+    public static <E> void main(String[] args) {
+        List<Integer> list = new ArrayList<Integer>();
+//        list.add(1);
+        list.add(21);
+        list.add(3);
+        list.add(1);
+        list.add(5);
+        list.add(33);
+        list.add(7);
+        list.add(2);
+        list.add(11);
+        list.add(32);
+        list.add(27);
+        list.add(4);
+        list.add(0);
+        list.add(100);
+        list.add(50);
+        
+        for (Integer integer : list) {
+            System.out.print(integer+" ");
+        }
+        radixSort(list);
+        System.out.println();
+        for (Integer integer : list) {
+            System.out.print(integer+" ");
+        }
+        
+    }
+    public static void radixSort(List<Integer> list){
+        
+    }
 }
