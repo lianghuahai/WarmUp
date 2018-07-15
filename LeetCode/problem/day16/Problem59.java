c515b17eae7f9e91abf4ae97fa0bad70342d0134
lianghuahai <huahai.liang@stonybrook.edu>
2018-07-15 05:19:26 -0400

    day16

---
+++ LeetCode/problem/day16/Problem59.java
@@ -0,0 +1,54 @@
+package problem.day16;
+
+public class Problem59 {
+
+    public static void main(String[] args) {
+        int[][] generateMatrix = generateMatrix(3);
+        for (int[] is : generateMatrix) {
+            for (int i : is) {
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+
+    }
+    public static int[][] generateMatrix(int n) {
+        int [][]matrix = new int [n][n];
+        if(n==0){
+            return matrix;
+        }
+        int left = 0, up =0,  down= matrix.length, right= matrix[0].length;
+        int value = 1;
+        while(left<right && up<down){
+            if(value-1==(matrix.length*matrix[0].length)){
+                break;
+            }
+            for(int i = left;i<right;i++){
+                matrix[up][i]=value++;
+            }
+            if(value-1==(matrix.length*matrix[0].length)){
+                break;
+            }
+            up++;
+            for(int i = up;i<down;i++){
+                matrix[i][right-1]=value++;
+            }
+            if(value-1==(matrix.length*matrix[0].length)){
+                break;
+            }
+            right--;
+            for(int i = right-1;i>=left;i--){
+                matrix[down-1][i]=value++;
+            }
+            if(value-1==(matrix.length*matrix[0].length)){
+                break;
+            }
+            down--;
+            for(int i = down-1;i>=up;i--){
+                matrix[i][left]=value++;
+            }
+            left++;
+        }
+        return matrix;
+    }
+}
