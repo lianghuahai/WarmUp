b9aa622dc514791b94900d0552e7bacdbb1e3161
lianghuahai <huahai.liang@stonybrook.edu>
2018-08-18 20:35:43 -0400

    a

---
+++ LeetCode/problem/day16/Problem66.java
@@ -0,0 +1,49 @@
+package problem.day16;
+
+public class Problem66 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+  //method 1.
+    public int[] plusOne1(int[] digits) {
+        for(int i =digits.length-1;i>=0;i--){
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
+            }else{
+                digits[i]=0;
+            }
+        }
+        int []res = new int[digits.length+1];
+        res[0]=1;
+        return res;
+   }
+    //method 2. foolish!
+    public int[] plusOne(int[] digits) {
+        if(digits[0]==0){
+            digits[0]=1;
+            return digits;
+        }
+        for(int i =digits.length-1;i>=0;i--){
+            if(digits[i]+1>=10){
+                digits[i]=0;
+            }else{
+                digits[i]=digits[i]+1;
+                break;
+            }
+        }
+        int[] nums = new int[digits.length+1];
+        if(digits[0]==0){
+            nums[0]=1;
+            for(int i =1;i<nums.length;i++){
+                nums[i]=0;
+            }
+            return nums;
+        }
+        
+        return digits;
+        
+    }
+}
---
+++ LeetCode/problem/day18/Problem64.java
@@ -0,0 +1,23 @@
+package problem.day18;
+
+public class Problem64 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+    /*
+     *  grid[i][j] = min(grid[row-1][col],grid[row][col-1])   +grid[row][col];
+     * 
+     */
+    public int minPathSum(int[][] grid) {
+        for(int row = 0;row<grid.length;row++){
+            for(int col = 0;col<grid[0].length;col++){
+                 if(row==0 && col !=0)grid[row][col]=grid[row][col]+grid[row][col-1];
+                 if(row!=0 && col ==0)grid[row][col]=grid[row][col]+grid[row-1][col];
+                 if(row!=0 && col !=0)grid[row][col]=Math.min(grid[row-1][col],grid[row][col-1])+grid[row][col];
+           } 
+        }
+      return grid[grid.length-1][grid[0].length-1];
+  }
+}
