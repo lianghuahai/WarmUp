1227807e9f6f720a81708b8d6cee8d40728349d7
lianghuahai <huahai.liang@stonybrook.edu>
2018-08-28 02:18:45 -0400

    a

---
+++ LeetCode/problem/day21/Problem132.java
@@ -0,0 +1,12 @@
+package problem.day21;
+
+public class Problem132 {
+/*      dp[i][j] = min cut from i to j making string palindrome. eg:  string="abc" , dp[0][2] =2 : a|b|c
+        if isPalindrome(i,j) dp[i][j]=0
+           else  dp[i][j] = 1+ min(dp[i][k] +dp[k+1][j]) k =i->j-1 
+  
+  
+  
+ */
+
+}
