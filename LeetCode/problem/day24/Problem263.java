c4da7e9098b09378ad1c6f3f1d0ec70359f92647
lianghuahai <huahai.liang@stonybrook.edu>
2018-09-15 23:14:09 -0400

    a

---
+++ LeetCode/problem/day24/Problem263.java
@@ -0,0 +1,12 @@
+package problem.day24;
+
+public class Problem263 {
+    public boolean isUgly(int num) {
+        if(num==1) return true;
+        if(num==0) return false;
+        while(num%2==0) num=num>>1;
+        while(num%3==0) num=num/3;
+        while(num%5==0) num=num/5;
+        return num==1;
+    }
+}
