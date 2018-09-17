dea10308aabe5be6a1ffe143452fd796ae87d41f
lianghuahai <huahai.liang@stonybrook.edu>
2018-09-17 12:41:30 -0400

    a

--- LeetCode/problem/day3/Problem36.java
+++ LeetCode/problem/day3/Problem36.java
@@ -1,9 +1,28 @@
 package problem.day3;
 
 import java.util.HashSet;
+import java.util.Set;
 
 public class Problem36 {
+    //easy understanding
     public boolean isValidSudoku(char[][] board) {
+        Set<String> seen = new HashSet<>();
+        for (int i=0; i<9; ++i) {
+            for (int j=0; j<9; ++j) {
+                char number = board[i][j];
+                if (number != '.')
+                    if (!seen.add(number + " in row " + i) ||
+                        !seen.add(number + " in column " + j) ||
+                        !seen.add(number + " in block " + i/3 + "-" + j/3))
+                        return false;
+            }
+        }
+        return true;
+    }
+    
+    
+    //not really good
+    public boolean isValidSudoku2(char[][] board) {
         for(int i = 0; i<9; i++){
             HashSet<Character> rows = new HashSet<Character>();
             HashSet<Character> columns = new HashSet<Character>();
