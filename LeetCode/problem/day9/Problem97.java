3b37c57dabe564c38eb85a1c77f7983b857c8b67
lianghuahai <huahai.liang@stonybrook.edu>
2018-09-20 01:58:04 -0400

    a

--- LeetCode/problem/day9/Problem97.java
+++ LeetCode/problem/day9/Problem97.java
@@ -3,14 +3,59 @@ package problem.day9;
 import java.util.HashMap;
 
 public class Problem97 {
+    //s1, s2只有两个字符串，因此可以展平为一个二维地图，判断是否能从左上角走到右下角。
+   /*       0  a  b    s1="aa",s2="ab",s3="aaba"
+        0   1  1  0
+        a   1  1  1
+        a   1  0  1
+        
+   In this array dp[i][j]dp[i][j] implies if it is possible to obtain a substring of length (i+j+2)(i+j+2) 
+   which is a prefix of s3s3 by some interleaving of prefixes of strings s1s1 and s2s2 having lengths (i+1)(i+1) 
+   and (j+1)(j+1) respectively. 
+   For filling in the entry of dp[i][j]dp[i][j], we need to consider two cases:     
+        
+  */
+    //1d array
+    public boolean isInterleave1(String s1, String s2, String s3) {
+        if (s3.length() != s1.length() + s2.length()) {
+            return false;
+        }
+        boolean dp[] = new boolean[s2.length() + 1];
+        for (int i = 0; i <= s1.length(); i++) {
+            for (int j = 0; j <= s2.length(); j++) {
+                if (i == 0 && j == 0) {
+                    dp[j] = true;
+                } else if (i == 0) {
+                    dp[j] = dp[j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1);
+                } else if (j == 0) {
+                    dp[j] = dp[j] && s1.charAt(i - 1) == s3.charAt(i + j - 1);
+                } else {
+                    dp[j] = (dp[j] && s1.charAt(i - 1) == s3.charAt(i + j - 1)) || (dp[j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1));
+                }
+            }
+        }
+        return dp[s2.length()];
+    }
+    
+    // 2d array
     public boolean isInterleave(String s1, String s2, String s3) {
-        HashMap <Character,Integer> dict = new HashMap<>();
-        for(Character ch :s1.toCharArray()){
-            dict.put(ch,dict.getOrDefault(ch,0)+1);
+        if (s3.length() != s1.length() + s2.length()) {
+            return false;
         }
-        for(char ch :dict.keySet()){
-            if(dict.get(ch)!=0)return false;
+        boolean dp[][] = new boolean[s1.length() + 1][s2.length() + 1];
+        for (int i = 0; i <= s1.length(); i++) {
+            for (int j = 0; j <= s2.length(); j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = true;
+                } else if (i == 0) {
+                    dp[i][j] = dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1);
+                } else if (j == 0) {
+                    dp[i][j] = dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1);
+                } else {
+                    dp[i][j] = (dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1)) || (dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1));
+                }
+            }
         }
-        return true;
+        return dp[s1.length()][s2.length()];
     }
 }
