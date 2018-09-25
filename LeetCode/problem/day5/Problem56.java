87c1dc6ba71b7098b95d63e92a2c156661992073
lianghuahai <huahai.liang@stonybrook.edu>
2018-09-25 12:39:04 -0400

    a

--- LeetCode/problem/day5/Problem56.java
+++ LeetCode/problem/day5/Problem56.java
@@ -14,58 +14,86 @@ public class Problem56 {
 
     }
     /* 这道题目给了我们一个区间的list，让我们返回一个list，是合并了所有有重叠的区间之后的list。
-                  这道题目的关键在于如何判断两个区间有重叠，根据原题给的例子可以看出，在按照每个区间的start排序之后很容易判断出是否相邻的两个区间有交集。
-                  我们看第一个区间[1,3] 中的end 3 > 第二个区间[2,6] 的start 2。在排序完之后，只要前面一个区间的end 大于等于 后面一个区间的start，它们就是重叠的。
-                  那么为了merge 这两个区间，保留第一个区间的start， 在两个end里面拿大的。设一个temp 等于第一个区间，遍历剩下的区间。
-                  然后每次拿temp 和这个区间比较，要注意的是，如果遇到了重叠的，把temp 更新为merge 后的区间，这时不需要加入list，因为还有可能继续和下一个区间重叠。
-                  当temp和这个区间不重叠的时候，把temp 加入list，再把这个区间设为新的temp
-   */
-    /*
-      comparetor  接口:
-                          该接口用于对象与对象间的比较，返回值意味着这两对象之间的大小关系。
-          -1代表前者小，0代表两者相等，1代表前者大。从小到大排列
+                这道题目的关键在于如何判断两个区间有重叠，根据原题给的例子可以看出，在按照每个区间的start排序之后很容易判断出是否相邻的两个区间有交集。
+                我们看第一个区间[1,3] 中的end 3 > 第二个区间[2,6] 的start 2。在排序完之后，只要前面一个区间的end 大于等于 后面一个区间的start，它们就是重叠的。
+                那么为了merge 这两个区间，保留第一个区间的start， 在两个end里面拿大的。设一个temp 等于第一个区间，遍历剩下的区间。
+                然后每次拿temp 和这个区间比较，要注意的是，如果遇到了重叠的，把temp 更新为merge 后的区间，这时不需要加入list，因为还有可能继续和下一个区间重叠。
+                当temp和这个区间不重叠的时候，把temp 加入list，再把这个区间设为新的temp
+            */
+            /*
+            comparetor  接口:
+                        该接口用于对象与对象间的比较，返回值意味着这两对象之间的大小关系。
+            -1代表前者小，0代表两者相等，1代表前者大。从小到大排列
+            */
+    public static List<Interval> merge3(List<Interval> intervals) {
+        // sort the intervals list according to start
+           List<Interval> res = new ArrayList<>();
+            Collections.sort(intervals, new Comparator<Interval>(){
+                public int compare(Interval a, Interval b)
+                {
+                    return a.start - b.start;
+                }
+            });;
+            
+           // get first one
+            Interval temp = intervals.get(0);
+            
+            // if intervals only has one element
+            if(intervals.size() == 1)
+            {
+                res.add(temp);
+                return res;
+            }
+            
+            // iterate intervals from [1] to end
+            for(int i=1; i<intervals.size(); i++)
+            {
+                // case 1: if temp interval end >= this interval start -> 
+                //            merge tempStart, max(tempEnd, thisEnd) and make this merge one as new temp;
+                if(temp.end >= intervals.get(i).start)
+                {
+                    temp.end = Math.max(temp.end, intervals.get(i).end);
+                }
+                // case 2: if temp interval is not overlapping this interval ->
+                //            add temp into list and make this new temp
+                else
+                {
+                    res.add(temp);
+                    temp = intervals.get(i);
+                }
+            }
+            // add the last temp into list
+            res.add(temp);
+            
+    
+            return res;
+       }
+    
+    
+    /*  先排序，排序需要nlogn的时间  ，再进行n次比较 ，比较只有两种情况，1是start<之前的end，就是overlap ，update end、 2。不是overlap ，把之前的start和end存起来
+     * 
+     *  overlap      not overlap
+    eg:  |__|       |__|
+           |__|          |__| 
+   time O(nlogn)  space O(n)
     */
-    public static List<Interval> merge(List<Interval> intervals) {
-     // sort the intervals list according to start
+    public List<Interval> merge(List<Interval> intervals) {
+        if(intervals==null||intervals.size()<=1)return intervals;
+        Collections.sort(intervals,(a,b)->a.start-b.start);
+        int start = intervals.get(0).start;
+        int end = intervals.get(0).end;
         List<Interval> res = new ArrayList<>();
-         Collections.sort(intervals, new Comparator<Interval>(){
-             public int compare(Interval a, Interval b)
-             {
-                 return a.start - b.start;
-             }
-         });;
-         
-        // get first one
-         Interval temp = intervals.get(0);
-         
-         // if intervals only has one element
-         if(intervals.size() == 1)
-         {
-             res.add(temp);
-             return res;
-         }
-         
-         // iterate intervals from [1] to end
-         for(int i=1; i<intervals.size(); i++)
-         {
-             // case 1: if temp interval end >= this interval start -> 
-             //            merge tempStart, max(tempEnd, thisEnd) and make this merge one as new temp;
-             if(temp.end >= intervals.get(i).start)
-             {
-                 temp.end = Math.max(temp.end, intervals.get(i).end);
-             }
-             // case 2: if temp interval is not overlapping this interval ->
-             //            add temp into list and make this new temp
-             else
-             {
-                 res.add(temp);
-                 temp = intervals.get(i);
-             }
-         }
-         // add the last temp into list
-         res.add(temp);
-         
- 
-         return res;
+        
+        for(Interval interval : intervals){
+            if(interval.start<=end){
+                end=Math.max(end,interval.end);
+            }else{
+                res.add(new Interval(start,end));
+                start=interval.start;
+                end=interval.end ;
+            }
+        }
+        res.add(new Interval(start,end));
+        return res;
     }
 }
--- LeetCode/problem/day5/Problem57.java
+++ LeetCode/problem/day5/Problem57.java
@@ -1,4 +1,4 @@
-package problem.day18;
+package problem.day5;
 
 import helperClass.Interval;
 
